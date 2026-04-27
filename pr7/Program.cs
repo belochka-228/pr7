57ff5a452ddcb4b612dc9aa55c65252083f69882
belochka-228 <sofikrohmal@gmail.com>
2026-04-27 20:02:26 +0300

    Исправленный код Фибоначчи

--- pr7/Program.cs
+++ pr7/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pr7
 {
@@ -10,6 +6,30 @@ namespace pr7
     {
         static void Main(string[] args)
         {
+                int result = Fibonacci(5);
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Вычисляет n-е число последовательности Фибоначчи
+        /// </summary>
+        /// <param name="n">Порядковый номер числа Фибоначчи</param>
+        /// <returns>n-е число Фибоначчи</returns>
+        static int Fibonacci(int n)
+        {
+            Console.WriteLine("The output is: ");
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
         }
     }
 }
